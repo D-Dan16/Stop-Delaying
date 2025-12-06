ff74916581f2d923ebd00137bd096e14fc94e650
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-12-06 02:58:55 +0200

    Realized that an activity contains Fragments, and not the other way around, so refactored the entire project such that Home, Leaderboard and Settings are now Fragments instead, which are all inside the newly made activity called activity_main_app.xml.
    Also created the UI for a new ~fragment~ called Tasks.

---
+++ app/src/main/java/com/example/procrastination/adapters/TasksPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.procrastination.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.procrastination.activities.fragments.TaskListFragment;
+import com.example.procrastination.models.Task;
+
+public class TasksPagerAdapter extends FragmentStateAdapter {
+
+    private static final int NUM_TABS = 3;
+
+    public TasksPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return TaskListFragment.newInstance(Task.TaskStatus.TODO);
+            case 1:
+                return TaskListFragment.newInstance(Task.TaskStatus.COMPLETED);
+            case 2:
+                return TaskListFragment.newInstance(Task.TaskStatus.CANCELED);
+            default:
+                return TaskListFragment.newInstance(Task.TaskStatus.TODO);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return NUM_TABS;
+    }
+}
