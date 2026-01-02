61c7f8a5668aad80a8752f9ed70f91dcfb40539e
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2026-01-02 21:13:47 +0200

    deleting unnecessary folders (this time correctly)

---
+++ app/src/main/java/procrastination/ui/activities/MainApp.java
@@ -0,0 +1,80 @@
+package procrastination.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.procrastination.R;
+import procrastination.ui.fragments.HomeFragment;
+import procrastination.ui.fragments.LeaderboardFragment;
+import procrastination.ui.fragments.LinksFragment;
+import procrastination.ui.fragments.ProcrastinationFragment;
+import procrastination.ui.fragments.SettingsFragment;
+import procrastination.ui.fragments.TasksFragment;
+import procrastination.ui.fragments.TipsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainApp extends AppCompatActivity implements HomeFragment.OnHomeFragmentInteractionListener {
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    TasksFragment tasksFragment = new TasksFragment();
+    LeaderboardFragment leaderboardFragment = new LeaderboardFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+    ProcrastinationFragment procrastinationFragment = new ProcrastinationFragment();
+    LinksFragment linksFragment = new LinksFragment();
+    TipsFragment tipsFragment = new TipsFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_app);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        replaceFragment(homeFragment);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home) {
+                replaceFragment(homeFragment);
+                return true;
+            } else if (item.getItemId() == R.id.nav_tasks) {
+                replaceFragment(tasksFragment);
+                return true;
+            } else if (item.getItemId() == R.id.nav_leaderboard) {
+                replaceFragment(leaderboardFragment);
+                return true;
+            } else if (item.getItemId() == R.id.nav_settings) {
+                replaceFragment(settingsFragment);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onWhatIsProcrastinationClicked() {
+        replaceFragment(procrastinationFragment);
+    }
+
+    @Override
+    public void onLinksAndVideosClicked() {
+        replaceFragment(linksFragment);
+    }
+
+    @Override
+    public void onTipsAndTricksClicked() {
+        replaceFragment(tipsFragment);
+    }
+}
---
+++ app/src/main/java/procrastination/ui/fragments/HomeFragment.java
@@ -0,0 +1,78 @@
+package procrastination.ui.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.procrastination.R;
+import com.google.android.material.button.MaterialButton;
+
+public class HomeFragment extends Fragment {
+
+    private OnHomeFragmentInteractionListener mListener;
+
+    public interface OnHomeFragmentInteractionListener {
+        void onWhatIsProcrastinationClicked();
+        void onLinksAndVideosClicked();
+        void onTipsAndTricksClicked();
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MaterialButton btnWhatIsProcrastination = view.findViewById(R.id.btnWhatIsProcrastination);
+        MaterialButton btnLinksAndVideos = view.findViewById(R.id.btnLinksAndVideos);
+        MaterialButton btnTipsAndTricks = view.findViewById(R.id.btnTipsAndTricks);
+
+        btnWhatIsProcrastination.setOnClickListener(v -> {
+            if (mListener != null) {
+                mListener.onWhatIsProcrastinationClicked();
+            }
+        });
+
+        btnLinksAndVideos.setOnClickListener(v -> {
+            if (mListener != null) {
+                mListener.onLinksAndVideosClicked();
+            }
+        });
+
+        btnTipsAndTricks.setOnClickListener(v -> {
+            if (mListener != null) {
+                mListener.onTipsAndTricksClicked();
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnHomeFragmentInteractionListener) {
+            mListener = (OnHomeFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnHomeFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
